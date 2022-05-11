3a2b2af88b5e4230fe6fcfff1a8bca36ecc2d99f
Shawn Argent <kshawn001@gmail.com>
2022-05-11 09:06:53 -0700

    daily

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const usersRouter = require("./users-router");
-const exampleRouter = require("./example-router");
+const workout_daysRouter = require("./workout_days-router");
 
 router.use("/users", usersRouter);
-router.use("/example", exampleRouter);
+router.use("/workout_days-router", workout_daysRouter);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,4 +3,5 @@ const homeRouter = require("./home-router");
 const apiRouter = require("./api");
 router.use("/", homeRouter);
 router.use("/api", apiRouter);
+router.use('workout_days-router')
 module.exports = router;
