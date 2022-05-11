3ff11da8a756cf6e2b63a6f8119a585998cbd800
Ryan Saldaen <ryan.saldaen@gmail.com>
2022-05-11 10:48:34 -0700

    server code fix

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 require('dotenv').config();
-const session = require("express-session");
 const exphbs = require('express-handlebars');
 const express = require('express');
 const path = require('path');
+const sequelize = require('./config/connection');
 const router = require('./controllers');
 const helpers = require('./util/helpers');
 const sessionMiddleware = require('./config/session');
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-const hbs = exphbs.create({ helpers });
 
 // setup app middleware
 app.use(sessionMiddleware);
