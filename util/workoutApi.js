343a37d95c40884d55102ac6dab246a7eee6eaa1
Mohammad Nabi Noorzai <mnoorzai21@gmail.com>
2022-05-12 14:15:59 -0700

    fetch workout from wger api

--- util/workoutApi.js
+++ util/workoutApi.js
@@ -1,6 +1,17 @@
 const fetch = require('node-fetch')
 
-const findAllWorkouts = async() => {}
+// fetches and returns from wger api
+const findAllWorkouts = async() => {
+    const response = await fetch('https://wger.de/api/v2/workout/', {
+        method: 'GET',
+        headers: {
+            Authorization: `Token ${process.env.WORKOUT_API_KEY}`,
+            'Content-Type': 'application/json',
+        },
+    })
+    const data = await response.json()
+    return data
+}
 
 const findWorkoutById = async() => {}
 
