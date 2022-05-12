7a2c5e15a419c06b76a46a6bad8aa903bff07e7d
Mohammad Nabi Noorzai <mnoorzai21@gmail.com>
2022-05-12 16:51:24 -0700

    update the home page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
-const router = require("express").Router();
-const { User } = require("../models");
+const router = require('express').Router()
+const { User } = require('../models')
 
 // use withAuth middleware to redirect from protected routes.
 // const withAuth = require("../util/withAuth");
@@ -9,40 +9,46 @@ const { User } = require("../models");
 //   // ...
 // });
 
-router.get("/", async (req, res) => {
-  try {
-    let user;
-    if (req.session.isLoggedIn) {
-      user = await User.findByPk(req.session.userId, {
-        exclude: ["password"],
-        raw: true,
-      });
+router.get('/', async(req, res) => {
+    try {
+        let user
+        if (req.session.isLoggedIn) {
+            user = await User.findByPk(req.session.userId, {
+                exclude: ['password'],
+                raw: true,
+            })
+        }
+        res.render('home', {
+            title: 'Home Page',
+            isLoggedIn: req.session.isLoggedIn,
+            user,
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('⛔ Uh oh! An unexpected error occurred.')
     }
-    res.render("home", {
-      title: "Home Page",
-      isLoggedIn: req.session.isLoggedIn,
-      user,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("⛔ Uh oh! An unexpected error occurred.");
-  }
-});
+})
 
-router.get("/login", (req, res) => {
-  res.render("login", { title: "Log-In Page" });
-});
+router.get('/login', (req, res) => {
+    res.render('login', { title: 'Log-In Page' })
+})
 
-router.get("/signup", (req, res) => {
-  res.render("signup", { title: "Sign-Up Page" });
-});
+router.get('/signup', (req, res) => {
+    res.render('signup', { title: 'Sign-Up Page' })
+})
 
-router.get("/workoutform", (req, res) => {
-  res.render('workoutform', { title: "Workout Form Page" });
-});
+router.get('/workoutform', (req, res) => {
+    res.render('workoutform', {
+        title: 'Workout Form Page',
+        isLoggedIn: req.session.isLoggedIn,
+    })
+})
 
-router.get("/workout", (req, res) => {
-  res.render('workout', { title: "Workout Page" });
-});
+router.get('/workout', (req, res) => {
+    res.render('workout', {
+        title: 'Workout Page',
+        isLoggedIn: req.session.isLoggedIn,
+    })
+})
 
-module.exports = router;
+module.exports = router
